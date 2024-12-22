c49bc1a1e93b3b238fd6e562dd975d168378ebb1
RGB_Raccoon <chldusgh0497@gmail.com>
2024-12-22 21:08:27 +0900

    메인과 gitignore수정정

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // router 불러오기
 
-createApp(App).use(router).mount('#app'); // router 사용하기
+const app= createApp(App)
+app.use(router); // router 사용하기
+const kakaoKey = process.env.VUE_APP_KAKAO_JS_KEY;
+if (!window.Kakao.isInitialized()) {
+    window.Kakao.init(kakaoKey);
+    console.log('Kakao SDK 초기화 완료:', window.Kakao.isInitialized());
+}
+
+app.mount('#app');
